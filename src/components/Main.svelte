08a3acabce91ec0095c65487577830adea138210
DarkDev <darkdev_brand@riseup.net>
2022-10-07 18:47:40 +0300

    initiate all components

---
+++ src/components/Main.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class="row">
+	<div class="avatar" style="background-image: url();">
+
+	</div>
+	<div class="avatar" style="background-image: url();">
+
+	</div>
+</div>
+<style>
+	.avatar {
+		width: 360px;
+		height: 650px;
+		border: 2px solid #fff;
+		margin: 0 25px 0 0;
+	}
+</style>
