9e3bcdf431b9bf4f7ae61949e45bdc62453432a0
DarkDev <darkdev_brand@riseup.net>
2022-10-07 18:46:10 +0300

    alfa v. of TOC component

---
+++ src/components/TableOfСontents.svelte
@@ -0,0 +1,24 @@
+<script>
+import { TileGroup, RadioTile } from "carbon-components-svelte";
+    
+    
+
+
+    export let selectedSection, values;
+</script>
+
+
+
+<TileGroup style="max-width: 400px; width: 100%; margin-bottom: 30px;"
+	legend=""
+	on:select={({ detail }) => {
+        (selectedSection = detail)
+        console.log((selectedSection))
+    }}
+	>
+	{#each values as value}
+		<RadioTile {value} checked={selectedSection === value}>{value}</RadioTile>
+	{/each}
+	<!-- <RadioTile value="10"  disabled>{}</RadioTile> -->
+	<h2>{selectedSection}</h2>
+</TileGroup>
