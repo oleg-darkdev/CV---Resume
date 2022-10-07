d5870bcf3e859e499ea6c141cc71f7cdac57f499
DarkDev <darkdev_brand@riseup.net>
2022-10-07 18:45:50 +0300

    alfa v. of portfolio component

---
+++ src/components/Portfolio.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { Tile } from "carbon-components-svelte";
+
+
+const portfolioProjects = [
+	{
+		img: '',
+		text: '',
+		github: ''
+	},
+	{
+		img: '',
+		text: '',
+		github: ''
+	},
+	{
+		img: '',
+		text: '',
+		github: ''
+	},
+	{
+		img: '',
+		text: '',
+		github: ''
+	},
+	{
+		img: '',
+		text: '',
+		github: ''
+	},
+];
+</script>
+
+
+
+<div class="projects-wrap row flex-wrap">
+    {#each portfolioProjects as project}
+        <Tile style="background-image: url({project.img}); width: 432px; height: 226px; margin: 0.3%; "></Tile>
+        <!-- border: 2px solid #fff; -->
+    {/each}
+</div>
+
+<style>
+
+	.projects-wrap {
+		justify-content: space-between;
+		flex-direction: row;
+	}
+</style>
