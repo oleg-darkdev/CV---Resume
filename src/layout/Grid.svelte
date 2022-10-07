67334cac256bf93b20d1dc2b65d188821024816d
DarkDev <darkdev_brand@riseup.net>
2022-10-07 18:46:28 +0300

    grid layout for app

---
+++ src/layout/Grid.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Grid, Row, Column } from "carbon-components-svelte";
+</script>
+
+
+<Grid>
+  <Row  style="padding-top: 2%;">
+    <Column  sm={10} md={4} lg={{ span: 10, offset: 1 }}>
+        <slot name="content" />
+	</Column>
+    <Column sm={{ span: 12, offset: 0 }} md={1} lg={3} style="padding-top: 8%;">
+        <slot name="TOC" />
+    </Column>
+	</Row>
+</Grid>
