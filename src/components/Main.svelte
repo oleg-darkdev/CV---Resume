7349371f94d9b2835bb2106f9f3fd42aecf5f2ff
DarkDev <darkdev_brand@riseup.net>
2022-10-08 20:01:30 +0300

    fix height and bg-image for avatar

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,18 +3,23 @@
 </script>
 
 <div class="row">
-	<div class="avatar" style="background-image: url();">
+	<div class="avatar" style="background-image: url(https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/photo//my_photo_1.png);">
 
 	</div>
-	<div class="avatar" style="background-image: url();">
+	<div class="avatar" style="background-image: url(https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/photo/my_photo_3.jpg);">
 
 	</div>
 </div>
 <style>
 	.avatar {
-		width: 360px;
-		height: 650px;
+		min-width: 360px;
+		width: 100%;
+		max-width: 550px;
+		height: 550px;
 		border: 2px solid #fff;
 		margin: 0 25px 0 0;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
 	}
 </style>
