e106b312e3fd1c5aa5db15ae77dea039f274410c
DarkDev <darkdev_brand@riseup.net>
2022-10-08 20:27:47 +0300

    add param of align elements

--- src/layout/Grid.svelte
+++ src/layout/Grid.svelte
@@ -1,11 +1,13 @@
 <script>
     import { Grid, Row, Column } from "carbon-components-svelte";
+
+    export let justifyContent;
 </script>
 
 
-<Grid style="min-height:100vh; height: auto;">
+<Grid style="min-height:100vh; height: auto;" >
   <Row  style="padding-top: 2%;">
-    <Column style="display: flex; align-items: center;" sm={10} md={4} lg={{ span: 10, offset: 1 }}>
+    <Column style="display: flex; align-items: center; justify-content: {justifyContent}; " sm={10} md={4} lg={{ span: 10, offset: 1 }}>
         <slot name="content" />
 	</Column>
     <Column sm={{ span: 12, offset: 0 }} md={1} lg={3} style="padding-top: 8%;">
