bff52a63a3c134f62f8db68e6abf3246d74a72ca
DarkDev <darkdev_brand@riseup.net>
2022-10-08 19:59:46 +0300

    table for show info in about component

---
+++ src/components/Bio/TableInfo.svelte
@@ -0,0 +1,37 @@
+<script>
+  import {
+    DataTable,
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+    ToolbarMenu,
+    ToolbarMenuItem,
+    Button,
+  } from "carbon-components-svelte";
+
+let dataTable = [],
+    headersData = [{ key: "", value: ""}];
+let backbtn = false;
+export let dataHeadersComplete, dataComplete, title, description, params, showSection;
+</script>
+
+
+
+
+<DataTable style="min-width: 380px; max-width: 455px; margin: 7px;" 
+  headers={headersData}
+  rows={dataTable}
+>
+  <strong slot="title">{title}</strong>
+  <span slot="description" style="font-size: 1rem">
+    {description}
+  </span>
+  <Toolbar>
+    <ToolbarContent>
+        {#if backbtn}
+            <Button on:click={() => {dataTable= []; headersData = [{ key: "", value: ""}]; showSection.personal = true; showSection.edu = true; showSection.exp = true; showSection.vol = true; backbtn = false; }} kind="secondary">Back</Button>
+        {/if}
+        <Button on:click={() => {dataTable= dataComplete; headersData = dataHeadersComplete; showSection.personal = params[0]; showSection.edu = params[1]; showSection.exp = params[2]; showSection.vol = params[3]; backbtn = true; console.log(showSection)}}>More infos</Button>
+    </ToolbarContent>
+  </Toolbar>
+</DataTable>
