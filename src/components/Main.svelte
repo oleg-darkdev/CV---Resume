254eb40304cc24643e45a3e5440e48dba43233ea
DarkDev <darkdev_brand@riseup.net>
2022-10-08 21:39:43 +0300

    add basic information

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,24 +2,48 @@
 
 </script>
 
-<div class="row">
+<div class="row flex-wrap main-wrap" >
 	<div class="avatar" style="background-image: url(https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/photo//my_photo_1.png);">
 
 	</div>
-	<div class="avatar" style="background-image: url(https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/photo/my_photo_3.jpg);">
+	<div class="text" style="">
+	<h1 style="font-size: 90px; margin-bottom: 25px;">Hi 👐, I`m Oleg Medvedev</h1>
+	<h2> 🛠  Engineer</h2>
+	<h2>📟 IT enthusiast</h2>
+	<h2>🎲 Boardgames creator</h2>
+	<h2>👍 Supporter open-source</h2>
+	<h2>🤖 Founder of 44Hackerspace </h2>
+	<h2>🏴 Social Activist</h2>
+	<!-- <h3>💙 Comfortable in turquoise model of the organizations or organizations with a flat structure</h3> -->
 
+<!-- Opportunity to evolve , a chance to showcase my talents and creative thinking 🧠 -->
 	</div>
 </div>
 <style>
+
+	.main-wrap, .text {
+		justify-content: center;
+		align-content: center;
+	}
+	.avatar, .text {
+		min-width: 380px;
+		width: auto;
+		max-width: 400px;
+		height: 650px;
+		margin: 0 1vw  15px 0;
+	}
+
 	.avatar {
-		min-width: 360px;
-		width: 100%;
-		max-width: 550px;
-		height: 550px;
 		border: 2px solid #fff;
-		margin: 0 25px 0 0;
 		background-position: center;
 		background-repeat: no-repeat;
 		background-size: cover;
+	}
+
+	.text {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
+		/* justify-content: center; */
 	}
 </style>
