1cfaf5e19ba8d6260b3ee2d621002612ab84a0ba
DarkDev <darkdev_brand@riseup.net>
2022-10-11 17:45:20 +0300

    add show / hide logic

--- src/components/Portfolio.svelte
+++ src/components/Portfolio.svelte
@@ -1,20 +1,34 @@
 <script>
-	import Project from './Portfolio/Project.svelte';
+	import ProjectsWrap from './Portfolio/ProjectsWrap.svelte';
+		
 	import projectsData from '../lib/data/projects';
+
+	let showSections = {
+		boardgamesApp: false,
+		socialProjects: false,
+		designProjects: false,
+		opensourceNpm: false
+	}
+	
+		
 </script>
 
-<div class="projects-wrap row flex-wrap">
-    {#each projectsData as project}
-		<Project {project}/>
-    {/each}
+<div class="column">
+	<!-- && == true -->
+	{#if !showSections.socialProjects && !showSections.opensourceNpm && !showSections.designProjects  }
+		<ProjectsWrap titleText='🎲 App`s for boardgames ' projectslist={projectsData.boardgamesApp}  bind:showThisList={showSections.boardgamesApp}/>
+	{/if}
+	{#if !showSections.boardgamesApp && !showSections.opensourceNpm && !showSections.designProjects  }
+		<ProjectsWrap titleText='👥 Social projects' projectslist={projectsData.socialProjects}  bind:showThisList={showSections.socialProjects}/>
+	{/if}
+	{#if !showSections.socialProjects && !showSections.boardgamesApp && !showSections.designProjects  }
+		<ProjectsWrap titleText='🧩 Open source ' projectslist={projectsData.opensourceNpm} bind:showThisList={showSections.opensourceNpm}/>
+	{/if}
+	{#if !showSections.boardgamesApp && !showSections.opensourceNpm && !showSections.socialProjects  }
+		<ProjectsWrap titleText='🖍 Only design' projectslist={projectsData.design}  bind:showThisList={showSections.designProjects}/>
+	{/if}
 </div>
 
-
 <style>
-	.projects-wrap {
-		justify-content: center;
-		flex-direction: row;
-		align-content: center;
-		width: 100%;
-	}
+
 </style>
