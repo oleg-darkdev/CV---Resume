e4fdf04b53045e9fbcd606af04f7ac4a9df55e7c
DarkDev <darkdev_brand@riseup.net>
2022-10-11 17:44:27 +0300

    refactoring data structure

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -1,21 +1,172 @@
-const portfolioProjects = [
-	{
-		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.explicit.bing.net%2Fth%3Fid%3DOIP.cLFYh5JgUcvbxNL4oz9WjAHaGF%26pid%3DApi&f=1&ipt=eb214f3e3d3d8dc45712191b56a9d8f3c33715691ee6a36912e319a1813bfac9&ipo=images',
-		text: '',
-		bgColor: '#673AB7',
-		textColor: '#fff',
-		description: {
-			techonologyList: ['Js', ''],
-			github: '',
-			site: '',
-			text: [
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-			]
+const portfolioProjects = {
+	boardgamesApp: [
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/LGBT_app.png',
+			text: 'LGBTQ+ app',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/anarchy_app.png',
+			text: 'A app',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/belarus_app.png',
+			text: 'Belarus protests app',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/kanbanel_app.png',
+			text: 'Kanbanism app',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
 		}
-	}
-];
+	],
+	socialProjects: [
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/darkdev.png',
+			text: 'DarkDev project',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/stopwar.png',
+			text: '#StopWar',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/boadgames_library.png',
+			text: '44Cubes',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/hackerspace.png',
+			text: '44Hackerspace',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		}
+	],
+	design: [],
+	opensourceNpm: [
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/resume.png',
+			text: 'This resume app',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/pomodoro%20calculator.png',
+			text: 'Pomodoro calculator',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		}
+	]
+};
 
 
 
