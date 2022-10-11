ffef365a5332f7b5d1b012fa66a07d37a79555d2
DarkDev <darkdev_brand@riseup.net>
2022-10-11 18:47:45 +0300

    add codears profile

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -141,7 +141,23 @@ const portfolioProjects = {
 			description: {
 				techonologyList: ['Js', ''],
 				github: '',
-				site: '', 
+				site: '',
+				text: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/portfolio/pomodoro%20calculator.png',
+			text: 'Codewars',
+			bgColor: '#673AB7',
+			textColor: '#fff',
+			description: {
+				techonologyList: ['Js', ''],
+				github: '',
+				site: 'https://www.codewars.com/users/Oleg_DarkDev',
 				text: [
 					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
