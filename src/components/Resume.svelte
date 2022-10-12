f1e6d580a10bf81cd595d5450b889c0157e77d8e
DarkDev <darkdev_brand@riseup.net>
2022-10-12 11:13:09 +0300

    refactoring data, add css for mobile v. & a:hover

--- src/components/Resume.svelte
+++ src/components/Resume.svelte
@@ -1,81 +1,11 @@
 <script>
-  import { ContentSwitcher, Switch } from "carbon-components-svelte";
+	import { ContentSwitcher, Switch } from "carbon-components-svelte";	
+	import resumeData from '../lib/data/resume';
+	
+	
 
   let selectedIndex = 1;
 
-  const skillsList = [
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/HTML5.png',
-		text: 'HTML5'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/CSS3.png',
-		text: 'CSS3'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/github.png',
-		text: 'GITHUB'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/js.png',
-		text: 'Javascript'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/linux.png',
-		text: 'Linux'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/m-3.png',
-		text: 'M 3.0'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/nodeJs.png',
-		text: 'NodeJS'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/scrum.png',
-		text: 'Scrum'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/design.png',
-		text: 'Design'
-	}
-  ];
-
-const toolsList = [
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/XD.png',
-		text: 'adobe XD'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/holacracy.png',
-		text: 'Holacracy'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/mongodb.png',
-		text: 'Mongo DB'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/svelteJs.png',
-		text: 'Svelte JS'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/vueJs.png',
-		text: 'Vue JS'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/webpack.png',
-		text: 'Webpack'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/figma.png',
-		text: 'Figma'
-	},
-	{
-		img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/illustrator.png',
-		text: 'Illustrator'
-	},
-  ];
 </script>
 
 
@@ -86,10 +16,11 @@ const toolsList = [
     </ContentSwitcher>
 
     <div style="justify-content: {selectedIndex == 0 ? "start" : "end"};" class="row skill-toll-wrap flex-wrap">
-            {#each selectedIndex == 0 ? skillsList : toolsList as item}
-                <div  class="column skill-toll-container">
+            {#each selectedIndex == 0 ? resumeData.skillsList : resumeData.toolsList as item}
+                <div  class="column skill-toll-container" on:click={() => window.open(item.link, "_blank")}>
                     <div style="background-image: url({item.img});" class="skill-img"></div>
-                    <h2 style="text-align: center;">{item.text}</h2>
+                    <!-- <h2 style="text-align: center;">{item.text}</h2> -->
+					<a href={item.link}>{item.text}</a>
                 </div>
             {/each}
     </div>
@@ -98,28 +29,39 @@ const toolsList = [
 
 <style>
     .context-switcher-wrap {
-		width: 80%;
+		width: 100%;
 		display: flex;
 		align-content: center;
 		align-items: center;
 	}
 
     .skill-img {
-		/* background-color: #fff; */
-		width: 140px;
-		height: 140px;
-		/* border: 2px solid #fff; */
+		max-width: 140px;
+		max-height: 140px;
+		width: 34vw;
+		height: 34vh;
 		
-	}
-.skill-img:hover {
-		background-color: #fff;
 	}
 	.skill-toll-wrap {
 		margin: 15px 0  0 0;
-		width:100%;
+		width: 100%;
 	}
 	.skill-toll-container {
 		margin:  0 0.6%  25px 0.4%;
+		padding: 5px;
+		border-radius: 4px;
+
 	}
 
+	.skill-toll-container:hover {
+		background-color: #161616;
+		border: 2px solid #fff;
+	} 
+
+	a {
+		text-decoration: none;
+		color: #fff;
+		font-size: 26px;
+		text-align: center; 
+	}
 </style>
---
+++ src/lib/data/resume.js
@@ -0,0 +1,100 @@
+const resume = {
+	skillsList: [
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/HTML5.png',
+			text: 'HTML5',
+			link: 'https://en.wikipedia.org/wiki/HTML5'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/CSS3.png',
+			text: 'CSS3',
+			link: 'https://en.wikipedia.org/wiki/CSS#CSS_3'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/js.png',
+			text: 'Javascript',
+			link: 'https://en.wikipedia.org/wiki/JavaScript'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/nodeJs.png',
+			text: 'NodeJS',
+			link: 'https://nodejs.org/en/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/github.png',
+			text: 'GITHUB',
+			link: 'https://github.com/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/linux.png',
+			text: 'Linux',
+			link: 'https://en.wikipedia.org/wiki/Linux'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/linux.png',
+			text: 'Management',
+			link: ''
+		},
+
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/design.png',
+			text: 'Design',
+			link: ''
+		}
+	],
+	toolsList: [
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/scrum.png',
+			text: 'Scrum',
+			link: 'https://www.scrum.org/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/skills/m-3.png',
+			text: 'M 3.0',
+			link: 'https://management30.com/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/holacracy.png',
+			text: 'Holacracy',
+			link: 'https://www.holacracy.org/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/svelteJs.png',
+			text: 'Svelte JS',
+			link: 'https://svelte.dev/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/vueJs.png',
+			text: 'Vue JS',
+			link: 'https://vuejs.org/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/webpack.png',
+			text: 'Webpack',
+			link: 'https://webpack.js.org/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/mongodb.png',
+			text: 'Mongo DB',
+			link: 'https://www.mongodb.com/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/figma.png',
+			text: 'Figma',
+			link: 'https://www.figma.com/'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/XD.png',
+			text: 'Adobe XD',
+			link: 'https://www.adobe.com/products/xd.html'
+		},
+		{
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/CV---Resume/master/src/lib/images/resume/tools/illustrator.png',
+			text: 'Illustrator',
+			link: 'https://www.adobe.com/products/illustrator/free-trial-download.html'
+		}
+	]
+};
+
+
+export default resume;
